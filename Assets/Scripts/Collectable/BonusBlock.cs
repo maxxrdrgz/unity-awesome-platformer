058a6f5dce6b3c677c2d51054daa3c1805bd4bae
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-04-16 12:55:02 -0700

    added script that animates bonus blockp

---
+++ Assets/Scripts/Collectable/BonusBlock.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusBlock : MonoBehaviour
+{
+
+    public Transform bottom_Collision;
+    public LayerMask playerLayer;
+
+    private Animator anim;
+    private Vector3 moveDirection = Vector3.up;
+    private Vector3 originPosition;
+    private Vector3 animPosition;
+    private bool startAnim;
+    private bool canAnimate = true;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originPosition = transform.position;
+        animPosition = transform.position;
+        animPosition.y += 0.15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForCollision();
+        AnimateUpDown();
+    }
+
+    void CheckForCollision()
+    {
+        if (canAnimate)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(bottom_Collision.position, Vector2.down, 0.1f, playerLayer);
+            if (hit)
+            {
+                if (hit.collider.gameObject.tag == Tags.PLAYER_TAG)
+                {
+                    //increase scoree
+                    anim.Play("BlockIdle");
+                    startAnim = true;
+                    canAnimate = false;
+                }
+            }
+        }
+    }
+
+    void AnimateUpDown()
+    {
+        if (startAnim)
+        {
+            transform.Translate(moveDirection * Time.smoothDeltaTime);
+            if(transform.position.y >= animPosition.y)
+            {
+                moveDirection = Vector3.down;
+            }else if(transform.position.y <= originPosition.y)
+            {
+                startAnim = false;
+                //canAnimate = false;
+            }
+        }
+    }
+}
