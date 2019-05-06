82ed1aecbbc9cbca886cf8d39af0169913e5f913
maxxrdrgz <maxxrdrgz@gmail.com>
2019-05-06 16:23:19 -0700

    Added comments

--- Assets/Scripts/Boss/StoneScript.cs
+++ Assets/Scripts/Boss/StoneScript.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class StoneScript : MonoBehaviour
 {
-
-
-
-    // Start is called before the first frame update
+    /**
+        Once started, this script will call the Deactivate() function after
+        4 seconds.
+    */
     void Start()
     {
         Invoke("Deactivate", 4f);
         // invoke calls the specifed function after time specified
     }
 
+    /**
+        Disables the gameobject that this script is applied to.
+        (Stone in this case)
+    */
     void Deactivate()
     {
         gameObject.SetActive(false);
     }
 
+    /**
+        Detects collision with the player. If player has been collided with
+        disable the gameobject and deal damage to the player.
+    */
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == Tags.PLAYER_TAG)
