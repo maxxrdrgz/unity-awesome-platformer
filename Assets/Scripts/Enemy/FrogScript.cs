d0d98d425c26c0746e229043131b24fba6012d09
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-05-07 01:06:58 -0700

    added comments

--- Assets/Scripts/Enemy/FrogScript.cs
+++ Assets/Scripts/Enemy/FrogScript.cs
@@ -21,14 +21,19 @@ public class FrogScript : MonoBehaviour
     {
         anim = GetComponent<Animator>();
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         StartCoroutine(frogJumpCoroutine);
         player = GameObject.FindGameObjectWithTag(Tags.PLAYER_TAG);
     }
 
-    // Update is called once per frame
+    /**
+        Detects if touching player. If so, deal damage to player. Checks for
+        ground which would mean that the frog has finished it's jump animation.
+        This would cause the frogs parent gameobject to update to the frogs
+        current transform.position.
+    */
     void Update()
     {
         if (Physics2D.OverlapCircle(transform.position, 0.5f, playerLayer))
@@ -46,6 +51,13 @@ public class FrogScript : MonoBehaviour
 
     }
 
+    /** 
+        Plays the frog's left or right jump animation. Counts the number of
+        times the frog has jumped and then recursively starts the 
+        frogJumpCoroutine.
+
+        @retuns {IEnumerator} returns random time delay between 1 and 4 seconds
+    */
     IEnumerator FrogJump()
     {
         yield return new WaitForSeconds(Random.Range(1f, 4f));
@@ -63,6 +75,12 @@ public class FrogScript : MonoBehaviour
         StartCoroutine(frogJumpCoroutine);
     }
 
+    /** 
+        Called once the frog's jump animation has completed. Plays either the
+        left or right idle animation and resets the jump counter to 0 after
+        the frog as jumped 3 times. Then flips the sign of the scale to move
+        the frog in the opposite direction.
+    */
     void AnimationFinished()
     {
         animation_finished = true;
@@ -85,6 +103,10 @@ public class FrogScript : MonoBehaviour
         }
     }
 
+    /** 
+        Detected collision with the ground. If the ground is detected and the
+        jump animation had been started, call AnimationFinished().
+    */
     void DetectGround()
     {
         RaycastHit2D groundDetected = Physics2D.Raycast(gameObject.transform.position, Vector2.down, 0.4f, groundLayer);
